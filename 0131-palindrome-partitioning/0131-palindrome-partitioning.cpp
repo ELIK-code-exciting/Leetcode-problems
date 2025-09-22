20f2d234d5e425c9037a3e19f7b80070dcee5cc7
ELIK-code-exciting <kudashevrinat78@gmail.com>
2025-09-22 16:42:09 +0400

    Time: 41 ms (66.29%), Space: 57.7 MB (53.13%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPaLinDrom(string s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    void backtrack_131(vector<vector<string>>& palindromes, vector<string>& curr, string& s, int start) {
+        if (start == s.size()) {
+            palindromes.push_back(curr);
+            return;
+        }
+
+        for (int end = start; end < s.size(); end++) {
+            if (isPaLinDrom(s, start, end)) {
+                curr.push_back(s.substr(start, end - start + 1));
+                backtrack_131(palindromes, curr, s, end + 1);
+                curr.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> palindromes;
+        vector<string> curr;
+        backtrack_131(palindromes, curr, s, 0);
+        return palindromes;
+    }
+};
