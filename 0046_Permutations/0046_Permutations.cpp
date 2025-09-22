d9bfa3d980abdd2210a5a6cdaed9d2f23ab97130
ELIK-code-exciting <kudashevrinat78@gmail.com>
2025-09-22 18:01:59 +0400

    Create 0046_Permutations.cpp

---
+++ 0046_Permutations/0046_Permutations.cpp
@@ -0,0 +1,25 @@
+void backtrack_46(vector<int>& nums, vector<vector<int>>& itog, vector<bool>& used, vector<int>& curr) {
+        if (curr.size() == nums.size()) {
+            itog.push_back(curr);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (!used[i]) {
+                curr.push_back(nums[i]);
+                used[i] = true;
+
+                backtrack_46(nums, itog, used, curr);
+
+                used[i] = false;
+                curr.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> itog;
+        vector<bool> used(nums.size(), false);
+        vector<int> curr;
+        backtrack_46(nums, itog, used, curr);
+        return itog;
+    }
