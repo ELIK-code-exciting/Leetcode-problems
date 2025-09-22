0afcfe21ba449fedf9f7a28f4e5f5037a47d3c36
ELIK-code-exciting <kudashevrinat78@gmail.com>
2025-09-22 16:49:44 +0400

    Time: 3 ms (31.4%), Space: 10.2 MB (35.97%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void backtrack(vector<int> nums, int start, vector<int>& curr, vector<vector<int>>& itog) {
+        itog.push_back(curr);
+
+        for (int i = start; i < nums.size(); i++) {
+            curr.push_back(nums[i]);
+            backtrack(nums, i + 1, curr, itog);
+            curr.pop_back();
+        }
+    }
+
+    // [] [1] [2] [3] [1,2] [1,3] [2,3] [1,2,3].
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> itog;
+        vector<int> curr;
+        backtrack(nums, 0, curr, itog);
+        return itog;
+    }
+};
